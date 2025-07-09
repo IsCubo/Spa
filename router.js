face2e3e099a3b20ef8eddc91afcde42a2552968
shnaider jordan <shnaiderjordan04@outlook.com>
2025-07-09 09:11:23 -0500

    add validation to register.html

--- router.js
+++ router.js
@@ -1,48 +1,58 @@
-import Swal from 'sweetalert2';
-import { isAuthenticated } from './src/views/auth.js';
+import Swal from "sweetalert2";
+import { isAuthenticated } from "./src/views/auth.js";
 
 const routes = {
-  '#login': './src/views/login.html',
-  '#new-user': './src/views/new-user.html',
-  '#dashboard': './src/views/dashboard.html',
-  '#profile': './src/views/profile.html', // NUEVA RUTA
-  '#404': './src/views/404.html',
+  "#login": "./src/views/login.html",
+  "#new-user": "./src/views/new-user.html",
+  "#dashboard": "./src/views/dashboard.html", // NUEVAS RUTA
+  "#404": "./src/views/404.html",
+  "#register": "./src/views/register.html",
 };
 
 export async function router() {
-  const path = window.location.hash || '#login';
+  const path = window.location.hash || "#login";
   const view = routes[path];
 
   // Control de autenticación
-  if ((path === '#dashboard' || path === '#profile') && !isAuthenticated()) {
-    window.location.hash = '#login';
+  if ((path === "#dashboard" || path === "#register") && !isAuthenticated()) {
+    window.location.hash = "#login";
+    return;
+  }
+
+  if ((path === "#login" || path === "#new-user") && isAuthenticated()) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user.rol === "admin") {
+      window.location.hash = "#dashboard";
+      return;
+    }
+    window.location.hash = "#register";
     return;
   }
 
   // Si no existe la vista, redirigir
   if (!view) {
-    window.location.hash = '#404';
+    window.location.hash = "#404";
     return;
   }
 
   try {
     const res = await fetch(view);
     const html = await res.text();
-    const app = document.getElementById('app');
+    const app = document.getElementById("app");
     app.innerHTML = html;
 
     // Ejecuta scripts embebidos
-    const scripts = app.querySelectorAll('script');
+    const scripts = app.querySelectorAll("script");
     scripts.forEach((script) => {
       const code = `(function(){\n${script.textContent}\n})();`;
-      const newScript = document.createElement('script');
+      const newScript = document.createElement("script");
       newScript.textContent = code;
       document.body.appendChild(newScript);
       script.remove();
     });
-
   } catch (err) {
-    console.error('Error cargando vista:', err);
-    document.getElementById('app').innerHTML = '<h2>Error al cargar la vista.</h2>';
+    console.error("Error cargando vista:", err);
+    document.getElementById("app").innerHTML =
+      "<h2>Error al cargar la vista.</h2>";
   }
 }
